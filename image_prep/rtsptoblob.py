1e1831094adc675898f14358b9a59d590980d9cd
naivelogic <threetimberland@gmail.com>
2019-03-20 20:28:36 -0700

    Update rtsptoblob.py

--- image_prep/rtsptoblob.py
+++ image_prep/rtsptoblob.py
@@ -1,10 +1,33 @@
 import cv2
+import socket
+
+def getWlanIp():
+    #if(os.name == "nt") :
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    try:
+        # doesn't even have to be reachable
+        s.connect(('10.255.255.255', 1))
+        IP = s.getsockname()[0]
+        if("172" in IP):
+            print("Ip address detected is :: " + IP )
+            IP = '127.0.0.1'
+            print("Ip address changed to :: " + IP + "to avoid docker interface")
+        print("Ip address detected is :: " + IP )
+        
+    except:
+        IP = '127.0.0.1'
+    finally:
+        s.close()
+    return IP
+
+#capture_url = "rtsp://" + getWlanIp() + ":8900/live"
+capture_url = 'rtsp://10.106.19.211:8900/live'
 
 i = 0
 local_path = 'images/cans/'
 image_type = 'can'
 
-cap = cv2.VideoCapture('rtsp://10.106.19.211:8900/live')
+cap = cv2.VideoCapture(capture_url)
 while True:
     _, frame = cap.read()
     cv2.imshow('my webcam', frame)
