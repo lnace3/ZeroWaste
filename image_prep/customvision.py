293365f2c0557f043d00ce76e1676f23ab745d6f
naivelogic <threetimberland@gmail.com>
2019-04-16 23:46:38 -0700

    Update customvision.py

--- image_prep/customvision.py
+++ image_prep/customvision.py
@@ -60,30 +60,38 @@ label_dict = {'can': '485411e9-feee-4d83-af5b-b933c30a1bbb',
 
 
 # Update this with the path to where you downloaded the images.
-base_image_url = "<<image path>>"
+file_list = df.filename.unique()
 
-# Go through the data table above and create the images
-print ("Adding images...")
-tagged_images_with_regions = []
+start = 0
+for stop in range(50, len(file_list), 50):
+
+    #11312
+    #2771
+
+    filelist = file_list[start:stop]
+    tagged_images_with_regions = []
+    for file in filelist:
+        regions_ = []
+        for _,row in df[df.filename==file].iterrows():
+            #print(row)
+            regions_.append(Region(tag_id = label_dict[row['class']], 
+            left = row.left,
+            top = row.top, 
+            width = row.width,
+            height = row.height))
+
+        with open(base_image_url + row.filename, mode='rb') as image_contents:
+                tagged_images_with_regions.append(ImageFileCreateEntry(name = row.filename, 
+                contents=image_contents.read(), 
+                regions = regions_))
 
 
-for _, row in df1.iterrows():
-    regions = [Region(tag_id = label_dict[row['class']].id, 
-    left = row.left,
-    top = row.top, 
-    width = row.width,
-    height = row.height)]
+    ## Upload results
+    upload_result = trainer.create_images_from_files(project.id, images=tagged_images_with_regions)
+    if not upload_result.is_batch_successful:
+        print("Image batch upload failed.")
+        for image in upload_result.images:
+            print("Image status: ", image.status)
+        exit(-1)
 
-    with open(base_image_url + row.filename, mode='rb') as image_contents:
-        tagged_images_with_regions.append(ImageFileCreateEntry(name = row.filename, 
-        contents=image_contents.read(),
-        regions = regions))
-
-
-## UPLOAD IMAGES TO ENDPOITN CUSTOMVISON
-upload_result = trainer.create_images_from_files(project.id, images=tagged_images_with_regions)
-if not upload_result.is_batch_successful:
-    print("Image batch upload failed.")
-    for image in upload_result.images:
-        print("Image status: ", image.status)
-    exit(-1)
+    start =stop
